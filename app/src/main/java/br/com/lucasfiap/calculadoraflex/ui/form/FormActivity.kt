81f7298eb25c170160454e198f52926a80cbb74c
Logon Aluno <logonaluno@local.com>
2019-12-14 11:16:01 -0300

    Initial commit

---
+++ app/src/main/java/br/com/lucasfiap/calculadoraflex/ui/form/FormActivity.kt
@@ -0,0 +1,14 @@
+package br.com.lucasfiap.calculadoraflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.lucasfiap.calculadoraflex.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
+
